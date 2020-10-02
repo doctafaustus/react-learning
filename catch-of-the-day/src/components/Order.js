62c62675b5f343adfa4db51eca7cc362d71a2f6a
doctafaustus <clowd150@gmail.com>
2020-10-01 20:26:14 -0500

    Finish lesson 17

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-  render() {
+
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable= fish.status === 'available';
+    if (!isAvailable) {
+      return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
+    }
     return (
-      <div className="order">
-        Order!
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    )
+  };
+
+  render() {
+
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prev, current) => {
+      const fish = this.props.fishes[current];
+      const count = this.props.order[current];
+      const isAvailable = fish && fish.status === 'available';
+      
+      if (isAvailable) {
+        return prev + (count * fish.price);
+      }
+      return prev;
+    }, 0);
+
+
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+        </ul>
+
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     )
   }
